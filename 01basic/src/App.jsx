c4b7e012ca8e83435a4ffd92577daafb8c16c57c
singhabhishek22957 <singhabhishek22957@gmai.com>
2024-11-08 16:34:57 +0530

    Colors

---
+++ 01basic/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className=' text-abhi  font-bold bg-abhi-100 border-4 border-gray-900 '>Tailwind CSS</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 01basic/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    
+    extend: {
+      colors:{
+      abhi:{
+        100:"#f47a0e"
+      }
+
+    }
+
+    },
+  },
+  plugins: [],
+}
+
